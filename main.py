8b51ea30f032dbb53c22d2ca9a0e4a6f6fe000d7
pacod8 <pacodominguezg@gmail.com>
2021-11-03 19:46:55 +0100

    added gui

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
 from deep_translator import (GoogleTranslator)
 import random
-
+from tkinter import *
 
 lenguajes = GoogleTranslator.get_supported_languages()
 
-translated = input("Que quieres traducir?: ")
-veces = int(input("Cuantas veces?: "))
 
-for i in range(veces):
-    translated = GoogleTranslator(source='auto', target=random.choice(lenguajes)).translate(text=translated)
+def clicked():
+    translated = txt.get()
+    veces = int(num.get())
+    for i in range(veces):
+        translated = GoogleTranslator(source='auto', target=random.choice(lenguajes)).translate(text=translated)
 
-translated = GoogleTranslator(source='auto', target="es").translate(text=translated)
-print(translated)
+    translated = GoogleTranslator(source='auto', target="es").translate(text=translated)
+    print(translated)
+    lbl.configure(text=translated)
+
+window = Tk()
+
+window.title("Traductor")
+
+window.geometry('700x350')
+
+lbl1 = Label(window, text="Texto", font=("Arial Bold",15))
+
+lbl1.grid(column=1, row=2)
+
+txt = Entry(window,width=20)
+
+txt.grid(column=2,row=2)
+
+lbl1 = Label(window, text="Veces", font=("Arial Bold",15))
+
+lbl1.grid(column=1, row=3)
+
+num = Scale(window, from_=1, to=50, orient=HORIZONTAL)
+num.grid(column=2,row=3)
+
+btn = Button(window, text="Traducir", command=clicked)
+
+btn.grid(column=2, row=4)
+
+lbl = Label(window, font=("Arial Bold",20))
+
+lbl.grid(column=2, row=5)
+
+window.mainloop()
+
+
+
+
+
+
+
+
