22f39e700ab46b6a0a468a18785a0c4b5f15383d
pacod8 <pacodominguezg@gmail.com>
2021-11-03 20:25:49 +0100

    added list log

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 from deep_translator import (GoogleTranslator)
 import random
 from tkinter import *
+from tkinter import ttk
 
 lenguajes = GoogleTranslator.get_supported_languages()
 
+s = ttk.Style()
+s.theme_use('clam')
 
 def clicked():
+
     log = []
+    log_lan = []
     def log_fun():
         print(log)
-        listbox = Listbox(width=50)
-        listbox.insert(0, *log)
-        listbox.grid(column=3,row=4)
+        listbox = ttk.Treeview(window,column=("c1","c2"),show="headings",height=5)
+        listbox.column("# 1", anchor=CENTER)
+        listbox.heading("# 1", text="Idioma")
+        listbox.column("# 2", anchor=CENTER)
+        listbox.heading("# 2", text="Texto")
+
+        for i in range(len(log)):
+            listbox.insert("","end",text="1",values=(log_lan[i],log[i]))
+
+        listbox.grid(column=4,row=4)
+
+
     translated = txt.get()
     log.append(translated)
+    log_lan.append("spanish")
     veces = int(num.get())
+
     for i in range(veces):
-        translated = GoogleTranslator(source='auto', target=random.choice(lenguajes)).translate(text=translated)
+        lan = random.choice(lenguajes)
+        translated = GoogleTranslator(source='auto', target=lan).translate(text=translated)
         log.append(translated)
+        log_lan.append(lan)
+
     translated = GoogleTranslator(source='auto', target="es").translate(text=translated)
     log.append(translated)
+    log_lan.append("spanish")
     print(translated)
     lbl.configure(text=translated)
+
     btn_log= Button(window,text="Mostrar registro", command=log_fun)
     btn_log.grid(column=3,row=5)
 
